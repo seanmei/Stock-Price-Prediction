3cb07ee383ebf8be68c139d2a70f7f4e76fcc339
seanmei <seanmei0728@gmail.com>
2021-01-12 01:16:41 -0500

    model trainning

---
+++ app.py
@@ -0,0 +1,9 @@
+import math 
+import pandas_datareader as web 
+import numpy as np 
+import pandas as pd 
+from sklearn.preprocessing import MinMaxScaler
+from keras.models import Sequential
+from keras.layers import Dense, LSTM 
+import maplotlib.pylot as plt
+plt.style.use('fivethirtyeight')
